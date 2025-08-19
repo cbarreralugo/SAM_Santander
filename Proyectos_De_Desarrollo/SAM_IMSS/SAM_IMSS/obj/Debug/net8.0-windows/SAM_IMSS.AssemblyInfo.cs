082dd036e5d4e075c6e40f0076c32e3c81aafad3
jamm110580 <157777247+jamm110580@users.noreply.github.com>
2025-08-19 14:14:26 -0600

    Listo

--- Proyectos_De_Desarrollo/SAM_IMSS/SAM_IMSS/obj/Debug/net8.0-windows/SAM_IMSS.AssemblyInfo.cs
+++ Proyectos_De_Desarrollo/SAM_IMSS/SAM_IMSS/obj/Debug/net8.0-windows/SAM_IMSS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAM_IMSS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ffe6cfb5cb41e161eb3de6f0894eaead0ec31d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAM_IMSS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAM_IMSS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
