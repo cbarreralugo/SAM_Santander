0646d7ce4ba7a07b111e2e77e004e13f2fa87284
jamm110580 <163212198+cbarreralugo@users.noreply.github.com>
2025-09-01 19:43:49 -0600

    IMSS_WEB

---
+++ Archivos_Compartidos/IMSS/Pruebas de Querys.sql
@@ -0,0 +1,37 @@
+
+
+EXEC SAM_IMSS.DBO.IMSS_ArcPosicionValuada @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosicionValuada @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcPosicionValuada @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosicionValuada @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
+
+EXEC SAM_IMSS.DBO.IMSS_ArcPosicionValuada_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosicionValuada_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcPosicionValuada_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosicionValuada_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
+
+EXEC SAM_IMSS.DBO.IMSS_ArcPosLayout @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosLayout @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcPosLayout @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosLayout @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
+
+EXEC SAM_IMSS.DBO.IMSS_ArcPosLayout_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosLayout_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcPosLayout_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcPosLayout_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
+
+EXEC SAM_IMSS.DBO.IMSS_ArcTrades @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcTrades @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcTrades @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcTrades @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
+
+EXEC SAM_IMSS.DBO.IMSS_ArcTrades_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcTrades_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041626'
+
+EXEC SAM_IMSS_1627.DBO.IMSS_ArcTrades_CSV @FechaReporte = '2025-09-01'
+EXEC SAM_IMSS_WEB.DBO.IMSS_ArcTrades_CSV @FechaReporte = '2025-09-01', @Contrato = 'MX40041627'
